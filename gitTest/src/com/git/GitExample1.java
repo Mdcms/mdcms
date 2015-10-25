215485af3560c77acc92d84cc4e12a89718fb996
Michael <mmorgan@mdcms.ch>
2015-10-25 09:46:17 +0100

    first test

---
+++ gitTest/src/com/git/GitExample1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.git;
+
+/**
+ * @author Michael
+ *
+ */
+public class GitExample1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
